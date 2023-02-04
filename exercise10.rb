a619c8d4d50260725248046f220be3c3b34cecaa
VIBHAV19 <52324572+VIBHAV19@users.noreply.github.com>
2023-02-04 23:01:26 +0530

    Modified code to resolve the comment :   avoid css

--- exercise10.rb
+++ exercise10.rb
@@ -1,6 +1,6 @@
 require 'selenium-webdriver'
 
-class Exercise
+class GetColumn
 
     def setup
         Selenium::WebDriver::Firefox::Service.driver_path = "/home/vibhav/Desktop/INFUSE/driver/geckodriver"
@@ -8,20 +8,24 @@ class Exercise
         @url = "https://computer-database.gatling.io/computers"
     end
 
-    def action(val)
+    def printText(val)
         wait = Selenium::WebDriver::Wait.new(:timeout => 30)
         begin
             @driver.get(@url)
             wait.until { @driver.find_element(:id, "main") }
+            rows = @driver.find_elements(:tag_name, "tr")
             cols = @driver.find_elements(:tag_name, "th")
             if val < 0 or val > cols.length
                 raise "Index out of bound error"
             end
-            puts @driver.find_element(:css, "thead tr th:nth-of-type(#{val}) a").text
-            columns = @driver.find_elements(:css, "tbody tr td:nth-of-type(#{val})")
-            columns.each do |column| 
-                puts column.text
-            end 
+             rows.each_with_index do |row, index|
+                columns = row.find_elements(:tag_name, "td")
+                columns.each_with_index do |column, index|
+                    if index == val
+                        puts column.text
+                    end
+                end
+            end
         rescue Exception => e
             e.message
         ensure
@@ -32,6 +36,6 @@ class Exercise
 
 end
 
-obj = Exercise.new
-obj.setup
-obj.action(1)
+getColumnOject = GetColumn.new
+getColumnOject.setup
+getColumnOject.printText(1)
