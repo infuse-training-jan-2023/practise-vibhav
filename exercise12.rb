0bb2b71dfa220522d0ba1e8fd0bcc74c55b8b609
VIBHAV19 <52324572+VIBHAV19@users.noreply.github.com>
2023-02-06 12:27:37 +0530

    Removed css

--- exercise12.rb
+++ exercise12.rb
@@ -16,9 +16,9 @@ class Exercise
             @wait.until{ @driver.find_element(:tag_name,"body")}
             search = @driver.find_element(:name,"search_key")
             search.send_keys(hotel + to_s(:enter))
-            option = @wait.until{ @driver.find_element(:css,"html body div.wrapper div.container-fluid.listing-wapper.listing-option3 div.row div.col-md-12 div.row div.col-md-12 div.row div.col-md-12.text-center form label.radio-inline input")}
+            option = @wait.until{ @driver.find_element(:name,"option")}
             option.click
-            @driver.find_element(:css,".panel-body > h5:nth-child(13) > a:nth-child(1)").click
+            @driver.find_element(:tag_name,"a").click
             puts "Checkout the hotel"
         rescue Exception => e
             puts e.message
@@ -27,12 +27,12 @@ class Exercise
     end
 
     def getBeaches
-        menu = @wait.until{@driver.find_element(:css,"html body div.wrapper header div.navbar div.navbar-collapse.collapse ul.nav.navbar-nav.navbar-right li.dropdown-aux a.navbar-toggle.navbar-toggle-aside-menu")}
+        menu = @wait.until{@driver.find_element(:class,"onscrollmenu")}
         menu.click
-        @driver.find_element(:css,".asideMenu-nav > ul:nth-child(1) > li:nth-child(6) > a:nth-child(1)").click
-        @driver.find_element(:css,".container-fluid > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > img:nth-child(1)").click
+        @driver.find_element(:xpath,"/html/body/div/section[1]/div/ul/li[5]/a").click
+        @driver.find_element(:class,"img-responsive").click
         puts "Beach Information:"
-        puts @driver.find_element(:css,".col-md-9 > div:nth-child(2) > div:nth-child(1) > p:nth-child(1)").text
+        puts @driver.find_element(:tag_name,"p").text
     end
 
     def finish
