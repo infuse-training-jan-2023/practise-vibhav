182a6719a543b751632dc643dbbe63517a95bba5
VIBHAV19 <52324572+VIBHAV19@users.noreply.github.com>
2023-02-07 17:47:23 +0530

    Changed method from get to post

--- src/exercise2.py
+++ src/exercise2.py
@@ -3,9 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/password/<string:password>', methods = ['GET'])
-def validate(password):
+@app.route('/password, methods = ['POST'])
+def validate():
     valid = r'^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$'
+    request_data = request.get_json()
+    password = request_data['password']
     if re.match(valid,password):
         return "Valid"
     return "Invalid"
