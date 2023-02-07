189556b8fadb2067eb1a2ccfc3afa8d45048c009
VIBHAV19 <52324572+VIBHAV19@users.noreply.github.com>
2023-02-07 16:04:57 +0530

    Updated as per comment -> followed dry approach

--- exercise2.py
+++ exercise2.py
@@ -1,13 +1,14 @@
 class ArrayManipulation:
-	
-
-    def element_at(arr,index):
-        try:
-            if len(arr) < 1 :
+		
+   def validate_input(arr,index):
+	if len(arr) < 1 :
                 raise AttributeError
             if index > len(arr) or index < 0:
                 raise IndexError 
-            
+	
+   def element_at(arr,index):
+        try:
+            validate_input(arr,index)
             return arr[index]
         except IndexError:
             return "Index value is out of bounds"
@@ -16,11 +17,7 @@ class ArrayManipulation:
 
     def inclusive_range(arr,start_pos,end_pos):
         try: 
-            if len(arr) < 1 :
-                raise AttributeError
-            if start_pos < 0 or start_pos > len(arr) or end_pos<0 or end_pos > len(arr):
-                raise IndexError
-            
+            validate_input(arr,index)
             return arr[start_pos:end_pos+1]
         except IndexError:
            return "Position value out of bound"
@@ -29,11 +26,7 @@ class ArrayManipulation:
     
     def non_inclusive_range(arr,start_pos,end_pos):
         try: 
-            if len(arr) < 1 :
-                raise AttributeError
-            if start_pos < 0 or start_pos > len(arr) or end_pos<0 or end_pos > len(arr):
-                raise IndexError
-            
+            validate_input(arr,index)
             return arr[start_pos:end_pos]
         except IndexError:
            return "Position value out of bound"
@@ -42,16 +35,9 @@ class ArrayManipulation:
     
     def start_and_length(arr,start_pos,length):
         try: 
-            if len(arr) < 1 :
-                raise AttributeError
-            if start_pos < 0 or start_pos > len(arr) or length<0 or length > len(arr):
-                raise IndexError
-            
+            validate_input(arr,index)
             return arr[start_pos:start_pos+length]
         except IndexError:
            return "Position value out of bound"
         except AttributeError:
             return "Array size should be greater than 0"
-      
-
-	
