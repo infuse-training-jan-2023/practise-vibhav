45c2d2600328fcc52001a8ae8deb0a28be19ade7
VIBHAV19 <52324572+VIBHAV19@users.noreply.github.com>
2023-02-07 17:08:50 +0530

    Changed as per comment

--- test_exercise3.py
+++ test_exercise3.py
@@ -8,7 +8,7 @@ class TestSerialAverage(unittest.TestCase):
     def test_skip_sports_positiveskip(self):
         obj = SportsArray
         output = obj.skip_sports(["Tennis","Football","Hocky","Cricket","Swimming"],2)
-        assert output == ['2:Hocky', '3:Cricket', '4:Swimming']
+        assertEquals(output,['2:Hocky', '3:Cricket', '4:Swimming'],"Both values are same")
         
     def test_skip_sports_negativeskip(self):
         obj = SportsArray
