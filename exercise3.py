ecd44d41308b28e636d9d29b6774de810759c7ec
VIBHAV19 <52324572+VIBHAV19@users.noreply.github.com>
2023-02-07 16:17:23 +0530

    Updated test3

--- exercise3.py
+++ exercise3.py
@@ -4,13 +4,13 @@ class SportsArray:
     def skip_sports(arr,skip):
         try:
             if len(arr) < 1 :
-                raise AttributeError
+                raise AttributeError("Index value is out of bounds")
             if skip > len(arr) or skip < 0:
-                raise IndexError 
+                raise IndexError("Array size should be greater than 0") 
             arr = [str(i)+":"+ arr[i] for i in range(0,len(arr))]
             return arr[skip:]
-        except IndexError:
-            return "Index value is out of bounds"
-        except AttributeError:
-            return "Array size should be greater than 0"
+        except IndexError as e:
+            return e
+        except AttributeError as a:
+            return a
 	
