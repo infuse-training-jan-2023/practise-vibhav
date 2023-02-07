267a2f3f9cddc0158701575bc97b7e7c272525c3
VIBHAV19 <52324572+VIBHAV19@users.noreply.github.com>
2023-02-07 17:43:46 +0530

    changed regex

--- src/exercise1.py
+++ src/exercise1.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def validate(mail):
     request_data = request.get_json()
     mail = request_data['mail']
-    valid = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+    valid = r'/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/'
     if re.match(valid,mail):
         return "Valid"
     return "Invalid"
