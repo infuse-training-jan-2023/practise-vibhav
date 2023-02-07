56c05cfea9eef2a114cc3bb5751fdef8a6f35c7d
VIBHAV19 <52324572+VIBHAV19@users.noreply.github.com>
2023-02-07 23:21:56 +0530

    Updated as per comment

--- workshop/main.rb
+++ workshop/main.rb
@@ -1,46 +1,51 @@
 require_relative "./selenium.rb"
 class LoginFlow
     extend SeleniumFramework
-
+    
+    
+    
+    def self.navigate_to_website
+        driver = setup("https://www.cocoleni.in/")
+    end
+    
     def self.login
-        setup("https://www.cocoleni.in/")
+        
         sleep(15)
         chk = checkDisplay(:class,"mc-modal-bg")
-        if chk == true
+        if chk 
             clickElement(:class,"mc-closeModal")
         end
  		clickElement(:xpath,"/html/body/header/nav/div/div/div/div[2]")
  		enterText(:id,"LoginForm_username","test@mail.com")
  		enterText(:id,"LoginForm_password","ttestword")
  		clickElement(:name,"yt0")
- 		done
     end
+    
 
     def self.signup
-        setup("https://www.cocoleni.in/")
+        #setup("https://www.cocoleni.in/")
         chk = checkDisplay(:class,"mc-modal-bg")
-        if chk == true
+        if chk 
             clickElement(:class,"mc-closeModal")
         end
  		clickElement(:xpath,"/html/body/header/nav/div/div/div/div[2]")
  		enterText(:id,"User_email","test@mail.com")
  		enterText(:id,"password","ttestword")
  		clickElement(:name,"yt1")
- 		done
     end
 
     def self.search
-        setup("https://www.cocoleni.in/")
+        #setup("https://www.cocoleni.in/")
         chk = checkDisplay(:class,"mc-modal-bg")
-        if chk == true
+        if chk 
             clickElement(:class,"mc-closeModal")
         end
         clickElement("xpath","/html/body/header/nav/div/div/div/div[1]")
         enterText(:id,"search","Novalis")
         enterText(:id,"search",:enter)
-        done
       
     end
+    quit_browser()
     
     
 end
