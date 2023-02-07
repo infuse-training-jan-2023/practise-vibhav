651436f8632545c6e1f6fa212fca75f3b0669d15
VIBHAV19 <52324572+VIBHAV19@users.noreply.github.com>
2023-02-07 16:43:24 +0530

    changed as per the comment assert => assertEqual

--- test_exercise1.py
+++ test_exercise1.py
@@ -9,34 +9,40 @@ class TestSerialAverage(unittest.TestCase):
 		obj = SerialAverage('abd-aa.ef-jj.li')
 		with pytest.raises(Exception) as e:  
 		    obj.getAverage()
-		    assert e.type == TypeError 
+		    message = "Both the values are equal"
+                    assertEqual(e.type, TypeError, message)
 
 	def test_alphanumericString(self):
 		obj = SerialAverage('002-aa.00-ss.00')
 		with pytest.raises(Exception) as e:  
 		    obj.getAverage()
-		    assert e.type == TypeError 
+		    message = "Both the values are equal"
+                    assertEqual(e.type, TypeError, message)
 
 	def test_numericString_proper_format(self):
 		obj = SerialAverage('002-10.00-20.00')
 		with pytest.raises(Exception) as e:  
 		    obj.getAverage()
-		    assert e.type == TypeError 
+		    message = "Both the values are equal"
+                    assertEqual(e.type, TypeError, message) 
 	
 	def test_numericString_improper_format(self):
 		obj = SerialAverage('002-15.00')
 		with pytest.raises(Exception) as e:  
 		    obj.getAverage()
-		    assert e.type == TypeError 
+		    message = "Both the values are equal"
+                    assertEqual(e.type, TypeError, message)
 
 	def test_numericString_noformat(self):
 		obj = SerialAverage('00215000909')
 		with pytest.raises(Exception) as e:  
 		    obj.getAverage()
-		    assert e.type == TypeError 
+		    message = "Both the values are equal"
+                    assertEqual(e.type, TypeError, message) 
 	
 	def test_AlphabeticalString_noformat(self):
 		obj = SerialAverage('gfjjdhksfksjfbwehdev')
 		with pytest.raises(Exception) as e:  
 		    obj.getAverage()
-		    assert e.type == TypeError 
+		    message = "Both the values are equal"
+                    assertEqual(e.type, TypeError, message)
