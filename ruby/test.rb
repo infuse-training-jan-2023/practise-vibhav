02dd44b43fb38530e4d2b6f332c79fe286dfe58e
VIBHAV19 <52324572+VIBHAV19@users.noreply.github.com>
2023-02-07 12:28:27 +0530

    Added  code to access env variable from dockerfile

--- ruby/test.rb
+++ ruby/test.rb
@@ -5,6 +5,9 @@ puts "Enter Your Name"
 input = gets
 puts "You Entered #{input}"
 
+
+puts "This script is written in "
+puts ENV["SCRIPTTYPE"]
 puts ENV["PROGRAM"]
 
 my_hash = JSON.parse('{"hello": "goodbye"}')
