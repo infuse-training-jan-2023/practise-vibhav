8abcce5d48d33cf52e5cb35701ccefa68bc54e70
VIBHAV19 <52324572+VIBHAV19@users.noreply.github.com>
2023-02-07 17:18:14 +0530

    Done change w.r.t obj

--- test_exercise3.py
+++ test_exercise3.py
@@ -4,27 +4,22 @@ import unittest
 
 from exercise3 import SportsArray
 class TestSerialAverage(unittest.TestCase):
-    
+    obj = SportsArray()
     def test_skip_sports_positiveskip(self):
-        obj = SportsArray
         output = obj.skip_sports(["Tennis","Football","Hocky","Cricket","Swimming"],2)
         assertEquals(output,['2:Hocky', '3:Cricket', '4:Swimming'],"Both values are same")
         
     def test_skip_sports_negativeskip(self):
-        obj = SportsArray
         with pytest.raises(Exception) as e:  
 		   obj.skip_sports(["Tennis","Football","Hocky","Cricket","Swimming"],-2)
 		    assert e.type == IndexError
     
     def test_skip_sports_greatestskip(self):
-        obj = SportsArray
         with pytest.raises(Exception) as e:  
 		    obj.skip_sports(["Tennis","Football","Hocky","Cricket","Swimming"],20)
 		    assert e.type == IndexError
 
     def test_skip_sports_emptyarray(self):
-        obj = SportsArray
         with pytest.raises(Exception) as e:  
 		    obj.skip_sports([],2)
 		    assert e.type == AttributeError
-
