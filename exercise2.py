cbcc36f6a264b6a77e041cf641139b058da8d9f5
VIBHAV19 <52324572+VIBHAV19@users.noreply.github.com>
2023-02-07 16:59:24 +0530

    changed as per comment

--- exercise2.py
+++ exercise2.py
@@ -2,42 +2,42 @@ class ArrayManipulation:
 		
    def validate_input(arr,index):
 	if len(arr) < 1 :
-                raise AttributeError
+                raise AttributeError("Array size should be greater than 0")
             if index > len(arr) or index < 0:
-                raise IndexError 
+                raise IndexError("Index value is out of bounds")
 	
    def element_at(arr,index):
         try:
             validate_input(arr,index)
             return arr[index]
-        except IndexError:
-            return "Index value is out of bounds"
-        except AttributeError:
-            return "Array size should be greater than 0"
+        except IndexError as i:
+            raise i
+        except AttributeError as a:
+            raise a
 
     def inclusive_range(arr,start_pos,end_pos):
         try: 
             validate_input(arr,index)
             return arr[start_pos:end_pos+1]
-        except IndexError:
-           return "Position value out of bound"
-        except AttributeError:
-            return "Array size should be greater than 0"
+        except IndexError as i:
+            raise i
+        except AttributeError as a:
+            raise a
     
     def non_inclusive_range(arr,start_pos,end_pos):
         try: 
             validate_input(arr,index)
             return arr[start_pos:end_pos]
-        except IndexError:
-           return "Position value out of bound"
-        except AttributeError:
-            return "Array size should be greater than 0"
+        except IndexError as i:
+            raise i
+        except AttributeError as a:
+            raise a
     
     def start_and_length(arr,start_pos,length):
         try: 
             validate_input(arr,index)
             return arr[start_pos:start_pos+length]
-        except IndexError:
-           return "Position value out of bound"
-        except AttributeError:
-            return "Array size should be greater than 0"
+        except IndexError as i:
+            raise i
+        except AttributeError as a:
+            raise a
